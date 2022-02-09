71659b7af74155f08bef8223bb338b6de6fdebb0
camin-mccluskey <c.mccluskey_7@hotmail.co.uk>
2022-02-09 18:51:04 +0000

    Migrate to TS

---
+++ index.ts
@@ -0,0 +1,23 @@
+type FixMeLater = {
+  [key: string]: any
+}
+
+
+export default function dotpath (str: string) {
+  const parts = str.toString().split('.')
+  const len = parts.length
+
+  return function parse (obj: FixMeLater) {
+    var testKey
+
+    for (var i = 0; i < len; ++i) {
+      testKey = parts[i]
+
+      if (!obj) return
+
+      obj = obj[testKey]
+    }
+
+    return obj
+  }
+}
